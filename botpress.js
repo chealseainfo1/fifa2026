b175a7ea19e556e6a7ba8ba4735693fc17aea8ad
Emmanuel Akaligwo <chealseainfo@yahoo.com>
2026-05-29 18:45:45 +0100

    Refactor Botpress initialization code

--- botpress.js
+++ botpress.js
@@ -1,24 +1,9 @@
-(function () {
-  const script = document.createElement("script");
-  script.src = "https://cdn.botpress.cloud/webchat/v1/inject.js";
-  script.async = true;
-
-  script.onload = () => {
-    window.botpressWebChat.init({
-      botId: "e6fd478c-6ebf-4c61-9a3f-cfc129897004",
-      clientId: "wkspace_01KSSZA5YSECHEQZB6E1W6SP7B",
-
-      hostUrl: "https://cdn.botpress.cloud/webchat/v1",
-
-      messagingUrl: "https://messaging.botpress.cloud",
-
-      configUrl: "https://files.bpcontent.cloud/2026/05/29/13/20260529133940-F5A1YCX8.json",
-
-      botName: "FIFA 2026 Analyst",
-      themeColor: "#0d6efd",
-      showPoweredBy: false
-    });
-  };
-
-  document.head.appendChild(script);
-})();
+window.botpressWebChat.init({
+  botId: "e6fd478c-6ebf-4c61-9a3f-cfc129897004",
+  clientId: "wkspace_01KSSZA5YSECHEQZB6E1W6SP7B",
+  hostUrl: "https://cdn.botpress.cloud/webchat/v1",
+  messagingUrl: "https://messaging.botpress.cloud",
+  botName: "FIFA 2026 Analyst",
+  themeColor: "#0d6efd",
+  showPoweredBy: false
+});
