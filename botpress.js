e6263111e2f1e364dfa78f9dd7f05f1a0fe2140e
Emmanuel Akaligwo <chealseainfo@yahoo.com>
2026-05-29 18:21:19 +0100

    Update bot ID and client ID in botpress.js

--- botpress.js
+++ botpress.js
@@ -5,8 +5,8 @@
 
   script.onload = () => {
     window.botpressWebChat.init({
-      botId: "kb_01KST83XZZW9VM0ATYZ9ETW6NT",
-      clientId: "YOUR_CLIENT_ID",
+      botId: "e6fd478c-6ebf-4c61-9a3f-cfc129897004",
+      clientId: "wkspace_01KSSZA5YSECHEQZB6E1W6SP7B",
       hostUrl: "https://cdn.botpress.cloud/webchat/v3.6/shareable.html?configUrl=https://files.bpcontent.cloud/2026/05/29/13/20260529133940-F5A1YCX8.json",
       messagingUrl: "https://messaging.botpress.cloud",
       botName: "FIFA 2026 Analyst",
