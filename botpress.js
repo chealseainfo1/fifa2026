cc4ebe5b99e1065a2394b533183550df79b3a5fa
Emmanuel Akaligwo <chealseainfo@yahoo.com>
2026-05-29 17:49:14 +0100

    Update Botpress script for new configuration

--- botpress.js
+++ botpress.js
@@ -5,9 +5,9 @@
 
   script.onload = () => {
     window.botpressWebChat.init({
-      botId: "YOUR_BOT_ID",
+      botId: "kb_01KST83XZZW9VM0ATYZ9ETW6NT",
       clientId: "YOUR_CLIENT_ID",
-      hostUrl: "https://cdn.botpress.cloud/webchat/v1",
+      hostUrl: "https://cdn.botpress.cloud/webchat/v3.6/shareable.html?configUrl=https://files.bpcontent.cloud/2026/05/29/13/20260529133940-F5A1YCX8.json",
       messagingUrl: "https://messaging.botpress.cloud",
       botName: "FIFA 2026 Analyst",
       themeColor: "#0d6efd",
