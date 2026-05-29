913eb0e0062457846df543107cb4f37c8991bc28
Emmanuel Akaligwo <chealseainfo@yahoo.com>
2026-05-29 18:52:23 +0100

    Refactor botpress initialization with async script loading
    Updated botpress initialization to load script asynchronously and added new configuration options.

--- botpress.js
+++ botpress.js
@@ -1,9 +1,25 @@
-window.botpressWebChat.init({
-  botId: "e6fd478c-6ebf-4c61-9a3f-cfc129897004",
-  clientId: "wkspace_01KSSZA5YSECHEQZB6E1W6SP7B",
-  hostUrl: "https://cdn.botpress.cloud/webchat/v1",
-  messagingUrl: "https://messaging.botpress.cloud",
-  botName: "FIFA 2026 Analyst",
-  themeColor: "#0d6efd",
-  showPoweredBy: false
-});
+(function () {
+  const script = document.createElement("script");
+  script.src = "https://cdn.botpress.cloud/webchat/v1/inject.js";
+  script.async = true;
+
+  script.onload = function () {
+    window.botpressWebChat.init({
+      botId: "e6fd478c-6ebf-4c61-9a3f-cfc129897004",
+      clientId: "wkspace_01KSSZA5YSECHEQZB6E1W6SP7B",
+
+      hostUrl: "https://cdn.botpress.cloud/webchat/v1",
+      messagingUrl: "https://messaging.botpress.cloud",
+
+      botName: "FIFA 2026 Analyst",
+      themeColor: "#0d6efd",
+      showPoweredBy: false,
+
+      enableReset: true,
+      showConversationsButton: true,
+      disableAnimations: false
+    });
+  };
+
+  document.head.appendChild(script);
+})();
