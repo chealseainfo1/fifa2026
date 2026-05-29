f6997c4ca768195bd5586ebe31201297e153542a
Emmanuel Akaligwo <chealseainfo@yahoo.com>
2026-05-29 18:30:51 +0100

    Update Botpress script to use new host URL

--- botpress.js
+++ botpress.js
@@ -7,8 +7,13 @@
     window.botpressWebChat.init({
       botId: "e6fd478c-6ebf-4c61-9a3f-cfc129897004",
       clientId: "wkspace_01KSSZA5YSECHEQZB6E1W6SP7B",
-      hostUrl: "https://cdn.botpress.cloud/webchat/v3.6/shareable.html?configUrl=https://files.bpcontent.cloud/2026/05/29/13/20260529133940-F5A1YCX8.json",
+
+      hostUrl: "https://cdn.botpress.cloud/webchat/v1",
+
       messagingUrl: "https://messaging.botpress.cloud",
+
+      configUrl: "https://files.bpcontent.cloud/2026/05/29/13/20260529133940-F5A1YCX8.json",
+
       botName: "FIFA 2026 Analyst",
       themeColor: "#0d6efd",
       showPoweredBy: false
